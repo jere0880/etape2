2c76f08669f9970f7371beb90fab04fd8eca57c5
François Gagnon <francois.gagnon.55@ulaval.cagit pushgit remote add origin https://github.com/jere0880/etape2.gitgit config --global user.email francois.gagnon.55@ulaval.ca>
2019-11-21 14:48:43 -0500

    Création de placer_mur

--- Quoridor.py
+++ Quoridor.py
@@ -43,6 +43,23 @@ class Quoridor:
             #Vérification du nombre de murs
             if joueur[0]['murs'] > 10 or joueur[1]['murs'] > 10 or joueur[0]['murs'] < 0 or joueur[1]['murs'] < 0:
                 raise QuoridorError('Nombre de murs invalides')
+            #Vérification de la position des murs
+            for l, mh in enumerate(murs['horizontaux']):
+                muh = murs['horizontaux'].copy()
+                del muh[l]
+                for mhp in muh:
+                    if mh[1] == mhp[1] and (mh[0] == mhp[0] or mh[0] == mhp[0] - 1 or mh[0] == mhp[0] + 1):
+                        raise QuoridorError('Position de murs invalide')
+                if mh[0] < 1 or mh[0] > 8 or mh[1] < 2 or mh[1] > 9:
+                    raise QuoridorError('Position de murs invalide')
+            for o, mv in enumerate(murs['verticaux']):
+                muv = murs['verticaux'].copy()
+                del muv[o]
+                for mvp in muv:
+                    if mv[0] == mvp[0] and (mv[1] == mvp[1] or mv[1] == mvp[1] - 1 or mv[1] == mvp[1] + 1):
+                        raise QuoridorError('Position de murs invalide')
+                if mv[0] < 2 or mv[0] > 9 or mv[1] < 1 or mv[1] > 8:
+                    raise QuoridorError('Position de murs invalide')
         
         #Vérification de la position des joueurs
         if joueur[0]['pos'][0] > 9 or joueur[1]['pos'][0] > 9 or joueur[0]['pos'][1] > 9 or joueur[1]['pos'][1] > 9:
@@ -51,7 +68,7 @@ class Quoridor:
             raise QuoridorError('Position invalide pour un joueur')
         self.murs = murs
         self.jeu = jeu
-        self.joueur = joueur
+        self.joueurs = joueur
     
     def __str__(self):
         nom = [self.jeu['joueurs'][0]['nom'], self.jeu['joueurs'][1]['nom']]
@@ -111,7 +128,35 @@ class Quoridor:
 
     #def partie_terminée(self):
    
-    #def placer_mur(self, njoueur, position, orientation):
+    def placer_mur(self, njoueur, position, orientation):
+        #Vérifie le numéro du joueur
+        if njoueur != 1 and njoueur != 2:
+            raise QuoridorError('le numéro du joueur est autre que 1 ou 2')
+        #Identifie le joueur
+        joueur = self.joueurs[njoueur - 1]
+        #Vérifie qu'il reste des murs à placer pour le joueur désigné
+        if joueur['murs'] == 0:
+            raise QuoridorError('le joueur a déjà placé tous ses murs')
+        #Vérifie que la place est libre pour le mur
+        for m in self.murs[orientation]:
+            if m[0] == position[0] and m[1] == position[1]:
+                raise QuoridorError('un mur occupe déjà cette position')
+            if orientation == 'horizontaux':
+                if m[1] == position[1] and (m[0] == position[0] or m[0] == position[0] - 1 or m[0] == position[0] + 1):
+                    raise QuoridorError('un mur occupe déjà cette position')
+            if orientation == 'verticaux':
+                if m[0] == position[0] and (m[1] == position[1] or m[1] == position[1] - 1 or m[1] == position[1] + 1):
+                    raise QuoridorError('un mur occupe déjà cette position')
+        #Vérifie que la position est valide
+        if orientation == 'horizontaux':
+            if position[0] < 1 or position[0] > 8 or position[1] < 2 or position[1] > 9:
+                raise QuoridorError('la position est invalide pour cette orientation')
+        if orientation == 'verticaux':
+            if position[0] < 2 or position[0] > 9 or position[1] < 1 or position[1] > 8:
+                raise QuoridorError('la position est invalide pour cette orientation')
+        #Place le mur et enlève un mur au joueur qui place le mur
+        joueur['murs'] -= 1
+        self.jeu['murs'][orientation].append(position)
 
 test = Quoridor(({"nom": "idul", "murs": 7, "pos": [5, 6]},
         {"nom": "automate", "murs": 3, "pos": [5, 7]}),{
@@ -119,3 +164,6 @@ test = Quoridor(({"nom": "idul", "murs": 7, "pos": [5, 6]},
         "verticaux": [[6, 2], [4, 4], [2, 5], [7, 5], [7, 7]]
     })
 print(test)
+Quoridor.placer_mur(test, 2, (3, 7), 'horizontaux')
+print(test)
+print(Quoridor.état_partie(test))
