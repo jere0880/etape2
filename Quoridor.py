095c1819e758a0cd767c32988fe3e32bf0d47eef
Jérémie Simard <jeremie.simard.1@ulaval.ca>
2019-11-21 11:01:36 -0500

    test 2

--- Quoridor.py
+++ Quoridor.py
@@ -1,3 +1,6 @@
+import network as nx
+
+
 class QuoridorError(Exception):
     def __init__(self, message):
         self.message = message
@@ -40,7 +43,7 @@ class Quoridor:
         self.jeu = jeu
     
     def __str__(self):
-        jeu = self.jeu
+    jeu = self.jeu
         nom = [jeu['joueurs'][0]['nom'], jeu['joueurs'][1]['nom']]
         table = []
         let = [['  |', '    ', '    ', '    ', '    ', '    ', '    ', '    ', '    ', '   |'],
@@ -93,11 +96,89 @@ class Quoridor:
        
     #def état_partie(self):
 
-    #def jouer_coup(self, joueur):
+    def jouer_coup(self, joueur):
+        graphe = construire_graphe(
+        [joueur['pos'] for joueur in état['joueurs']], 
+        état['murs']['horizontaux'],
+        état['murs']['verticaux']
+    )
+        nx.shortest_path(graphe, (5,6), 'B1')
 
-    #def partie_terminée(self):
+
+
+    def partie_terminée(self):
+
    
     #def placer_mur(self, joueur: int, position: tuple, orientation: str):
+
+
+def construire_graphe(joueurs, murs_horizontaux, murs_verticaux):
+
+    """
+    Crée le graphe des déplacements admissibles pour les joueurs.
+
+    :param joueurs: une liste des positions (x,y) des joueurs.
+    :param murs_horizontaux: une liste des positions (x,y) des murs horizontaux.
+    :param murs_verticaux: une liste des positions (x,y) des murs verticaux.
+    :returns: le graphe bidirectionnel (en networkX) des déplacements admissibles.
+    """
+    graphe = nx.DiGraph()
+
+    # pour chaque colonne du damier
+    for x in range(1, 10):
+        # pour chaque ligne du damier
+        for y in range(1, 10):
+            # ajouter les arcs de tous les déplacements possibles pour cette tuile
+            if x > 1:
+                graphe.add_edge((x, y), (x-1, y))
+            if x < 9:
+                graphe.add_edge((x, y), (x+1, y))
+            if y > 1:
+                graphe.add_edge((x, y), (x, y-1))
+            if y < 9:
+                graphe.add_edge((x, y), (x, y+1))
+
+    # retirer tous les arcs qui croisent les murs horizontaux
+    for x, y in murs_horizontaux:
+        graphe.remove_edge((x, y-1), (x, y))
+        graphe.remove_edge((x, y), (x, y-1))
+        graphe.remove_edge((x+1, y-1), (x+1, y))
+        graphe.remove_edge((x+1, y), (x+1, y-1))
+
+    # retirer tous les arcs qui croisent les murs verticaux
+    for x, y in murs_verticaux:
+        graphe.remove_edge((x-1, y), (x, y))
+        graphe.remove_edge((x, y), (x-1, y))
+        graphe.remove_edge((x-1, y+1), (x, y+1))
+        graphe.remove_edge((x, y+1), (x-1, y+1))
+
+    # retirer tous les arcs qui pointent vers les positions des joueurs
+    # et ajouter les sauts en ligne droite ou en diagonale, selon le cas
+    for joueur in map(tuple, joueurs):
+
+        for prédécesseur in list(graphe.predecessors(joueur)):
+            graphe.remove_edge(prédécesseur, joueur)
+
+            # si admissible, ajouter un lien sauteur
+            successeur = (2*joueur[0]-prédécesseur[0], 2*joueur[1]-prédécesseur[1])
+
+            if successeur in graphe.successors(joueur) and successeur not in joueurs:
+                # ajouter un saut en ligne droite
+                graphe.add_edge(prédécesseur, successeur)
+
+            else:
+                # ajouter les liens en diagonal
+                for successeur in list(graphe.successors(joueur)):
+                    if prédécesseur != successeur and successeur not in joueurs:
+                        graphe.add_edge(prédécesseur, successeur)
+
+    # ajouter les noeuds objectifs des deux joueurs
+    for x in range(1, 10):
+        graphe.add_edge((x, 9), 'B1')
+        graphe.add_edge((x, 1), 'B2')
+
+    return graphe
+
 
 test = Quoridor(({"nom": "idul", "murs": 7, "pos": [5, 6]},
         {"nom": "automate", "murs": 3, "pos": [5, 7]}),{
