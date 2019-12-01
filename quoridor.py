62ec5b391f847e7ffdf352aa64ae86b2e72f61f2
Jérémie Simard <jeremie.simard.1@ulaval.ca>
2019-12-01 13:46:37 -0500

    erreur mur invalide qui ne fonctionne pas

--- quoridor.py
+++ quoridor.py
@@ -352,8 +352,9 @@ def ajout_arcs(graphe, x, y):
     if y < 9:
         graphe.add_edge((x, y), (x, y+1))
 ###Programme###
-TEST = Quoridor(({"nom": "idul", "murs": 10, "pos": [5, 1]},
-                 {"nom": "automate", "murs": 10, "pos": [5, 9]}), {
-                     "horizontaux": [],
-                     "verticaux": []
+TEST = Quoridor(({"nom": "idul", "murs": 9, "pos": [4, 4]},
+                 {"nom": "automate", "murs": 9, "pos": [5, 9]}), {
+                     "horizontaux": [(0, 6)],
+                     "verticaux": [(5,5)]
                  })
+print(TEST)
